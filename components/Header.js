2fa022995ed03e78f08bee9cda8529e79bad6cd6
maikelordaz <maikelordaz@gmail.com>
2022-08-19 23:23:54 -0400

    Header

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import { ConnectButton } from "web3uikit"
+import Link from "next/link"
+
+export default function Header() {
+    return (
+        <nav>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+            <Link href="/sell-nft">
+                <a>Sell NFT</a>
+            </Link>
+            <ConnectButton />
+        </nav>
+    )
+}
