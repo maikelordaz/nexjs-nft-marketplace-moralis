b2bfb105227b3b421475b2c5ac8c7112b16b2c1f
maikelordaz <maikelordaz@gmail.com>
2022-08-22 23:22:24 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
-import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>NFT Marketplace</title>
-                <meta name="description" content="NFT Marketplace" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            Hi!
-        </div>
-    )
+    /*
+     * ¿Como mostramos la lista de los NFT?
+     * Indexaremos los eventos off-chain y los leeremos de nuestra base de datos
+     * Acomodaremos un servidor que escuche cuando se ejecuten estos eventos y los añadiremos
+     * a nuestra base de datos para buscar en ella. Asi no tenemos que modificar nuestro contrato/
+     * protocolo para adaptarlo a nuestra pagina.
+     * Esto puede ser
+     * Centralizado -> usando Moralis
+     * Descentralizado -> usando The Graph.
+     * En este ejemplo se usa Moralis. Aunque añadamos este componente centralizado, nuestra logica
+     * sigue siende descentralizada
+     * Los protocolos centralizados como Moralis, OpenSea, Etherscan, Coinmarket, Pinata, nos ayudan
+     * para hacer mas rapidos nuestras respuestas
+     */
+    return <div className={styles.container}>Hi!</div>
 }
