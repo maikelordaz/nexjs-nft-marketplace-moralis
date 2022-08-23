4b9c484242061b0bc1cba9f48697bb1564dcb770
maikelordaz <maikelordaz@gmail.com>
2022-08-22 23:23:07 -0400

    Create addEvents.js

---
+++ addEvents.js
@@ -0,0 +1,11 @@
+const Moralis = require("moralis/node")
+require("dotenv").config()
+
+async function main() {}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
