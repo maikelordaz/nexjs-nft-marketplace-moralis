aa5fb9dcc854fc99e2cf57ac990f8ccd289208c4
maikelordaz <maikelordaz@gmail.com>
2022-08-29 21:28:48 -0400

    Update with seller

--- components/NFTBox.js
+++ components/NFTBox.js
@@ -6,8 +6,26 @@ import Image from "next/image"
 import { Card } from "web3uikit"
 import { ethers } from "ethers"
 
+const truncateStr =
+    (fullStr,
+    (strLen) => {
+        if (fullStr.length <= strLen) return fullStr
+
+        const separator = "..."
+        const separatorLength = separator.length
+        const charsToShow = strLen - separatorLength
+        const frontChars = Math.ceil(charsToShow / 2)
+        const backChars = Math.floor(charsToShow / 2)
+        return (
+            fullStr.substring(0, frontChars) +
+            separator +
+            fullStr.substring(fullStr.length - backChars)
+        )
+    })
+
 export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress, seller }) {
-    const { isWeb3Enabled } = useMoralis()
+    // Con account tomo la cuenta del Metamask que este conectada
+    const { isWeb3Enabled, account } = useMoralis()
     const [imageURI, setImageURI] = useState("")
     const [tokenName, setTokenName] = useState("")
     const [tokeDescription, setTokenDescription] = useState("")
@@ -56,6 +74,9 @@ export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress,
         }
     }, [isWeb3Enabled])
 
+    const isOwnedByUser = seller === account || seller === undefined
+    const formattedSellerAddress = isOwnedByUser ? "you" : truncateStr(seller || "", 15)
+
     return (
         <div>
             <div>
@@ -64,7 +85,9 @@ export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress,
                         <div className="p-2">
                             <div className="flex flex-col items-end gap-2">
                                 <div>#{tokenId}</div>
-                                <div className="italic text-sm">Owned by {seller}</div>
+                                <div className="italic text-sm">
+                                    Owned by {formattedSellerAddress}
+                                </div>
                                 <Image
                                     loader={() => imageURI}
                                     src={imageURI}
